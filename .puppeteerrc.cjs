10411354a8ef3161c124961ed88a9b05417936e1
seeramsandeep <sandeep.seeram@kenpath.io>
2025-03-10 21:05:18 +0530

    Cleaned the files

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,7 +4,7 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'), // Custom cache directory (optional)
+  // cacheDirectory: join(__dirname, '.cache', 'puppeteer'), // Custom cache directory (optional)
   chrome: {
     skipDownload: false, // Ensure Chrome is downloaded
   },
